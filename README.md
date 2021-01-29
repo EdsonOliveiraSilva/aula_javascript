4e57c661d34a7e8931fe027f2198ac5bf22a6e92
Edson Oliveira <72816536+EdsonOliveiraSilva@users.noreply.github.com>
2021-01-28 23:50:32 -0300

    Primeiro commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # aula_javascript
-Introdução ao JavaScript
+Introdução ao JavaScript;
+Primeiros passos em JavaScript do Bootcamp da DIO;
+Array e Dicionario;
+Condicionais, laços de repetição e Date; 
---
+++ main.js
@@ -0,0 +1,185 @@
+/*
+function clicou(){
+  document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar!</b>";
+  //console.log(document.getElementById("agradecimento"));
+  //alert("Obrigado por clicar!");
+}
+
+function redirecionar(){
+  window.open("https://github.com/EdsonOliveiraSilva/aula_javascript"); //Outra aba!
+  //window.location.href ="https://www.linkedin.com/in/edson-oliver/"; //Mesma Janela!
+}
+
+function trocar(elemento){
+  elemento.innerHTML = "Obrigado por passar o mouse";
+  //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+  //alert("trocar texto");
+}
+
+function voltar(elemento){
+  elemento.innerHTML = "Passe o mouse aqui";
+  //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+}
+
+function load(){
+  alert("página carregada");
+}
+
+function funcaoChange(elemento){
+  console.log(elemento.value)
+}
+*/
+
+
+
+
+/* function soma(n1, n2){
+  return n1+n2;
+}
+
+function validaIdade(idade){
+  var validar;
+  if (idade >= 18){
+    validar = true
+  }
+  else{
+    validar = false
+  }
+  return validar;
+}
+
+var idade = prompt("Qual é a sua idade?");
+console.log(validaIdade(idade));
+*/
+
+/*
+function setReplace(frase, nome, novo_nome){
+  return frase.replace(nome, novo_nome)
+}
+alert(soma(5,10));
+alert(setReplace("Vai Japão", "Japão", "Brasil"));
+*/
+
+
+
+
+
+
+//var d = new Date();
+//alert(d.getUTCFullYear());
+
+//var d = new Date();
+//alert(d.getDay());
+
+//var d = new Date();
+//alert(d.getHours());
+
+//var d = new Date();
+//alert(d.getDate());
+
+//var d = new Date();
+//alert(d.getMinutes());
+
+//var d = new Date();
+// alert(d.getMonth()+1);
+
+//var d = new Date();
+//alert(d);
+
+
+
+
+
+/*
+var count;
+for(count=0; count <= 5; count++){
+  alert(count);
+};
+*/
+
+
+/*var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+};
+*/
+
+
+/*
+var count = 0;
+while (count < 5){
+    console.log(count);
+    count = count +1;
+};
+*/
+
+
+
+
+
+/*
+var idade = prompt("Qual é a sua idade?");
+if (idade >= 18){
+  alert("maior de idade");
+}
+else {
+  alert("menor de idade");
+};
+*/
+
+
+/*
+var idade = 18;
+if (idade > 18){
+  alert("maior de idade");
+}
+else {
+  alert("menor de idade");
+};
+*/
+
+
+
+
+
+//var livros = [{nome:"Narnia", autor:"C.S.Lewis"},{nome:"Hobbit", autor:"J.R.Tolkien"}];
+//console.log(livros);
+//alert(livros[1].autor);
+
+//var livros = {nome:"Narnia", autor:"C.S.Lewis"};
+//console.log(livros.nome);
+//alert(livros.autor);
+
+
+
+
+
+//var livros =["Narnia" , "LOTR", "Star Wars"];
+//livros.push("Hobbit");
+//livros.pop();
+//console.log(livros[0]);
+//console.log(livros.toString()[0]);
+//console.log(livros.join(" - "));
+//console.log(livros[1]);
+//alert(livros[1]);
+//console.log(livros.length);
+//console.log(livros.reverse());
+
+
+
+
+
+//var nome = "Edson Oliveira";
+//var idade = 36;
+//var idade2 = -13;
+//var frase = "One Piece é o melhor anime do  muundo";
+//alert(nome + " tem " + idade + " anos ");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(idade+idade2);  
+//console.log(frase.replace("One Piece", "Naruto"));
+//console.log(frase.toUpperCase("One Piece"));
+//console.log(frase.toLowerCase( "One Piece"));
+//alert(frase.replace("Naruto", "One Piece"));
